a164d1a077b0deabb3620c58634f8e271c3c5360
Lysak-Anna <hvlysak@gmail.com>
2023-07-20 00:07:06 +0300

    ADD: required fields, CHANGE: status values

--- models/orderModel.js
+++ models/orderModel.js
@@ -6,24 +6,27 @@ const orderSchema = new Schema(
     status: {
       type: String,
       enum: {
-        values: ["Pending", "Cooking", "Served"],
+        values: ["Open", "Paid"],
         message: "{VALUE} is not supported",
       },
-      default: "Pending",
+      default: "Open",
     },
     orderItems: [
       {
         dish_id: {
           type: mongoose.ObjectId,
           ref: "Dish",
+          required: [true, "At least one item is required"],
         },
         quantity: Number,
+
         _id: false,
       },
     ],
     table_id: {
       type: mongoose.ObjectId,
       ref: "Table",
+      required: [true, "Table id is required"],
     },
     waiter_id: {
       type: mongoose.ObjectId,
