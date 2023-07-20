93a53c50adbb12b260b912ce697ff91028be72ad
Olga Chernega <eliang.cheng@gmail.com>
2023-07-20 12:27:35 +0300

    fix: change schema to the common type with others

--- models/cookModel.js
+++ models/cookModel.js
@@ -3,25 +3,36 @@ const { Schema, model } = require("mongoose");
 const cookSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: [true, "Name is required"]
   },
   password: {
     type: String,
-    required: true
+    required: [true, "Password is required"],
+    validate: {
+      validator: function (password) {
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,30}$/.test(password);
+      },
+      message:
+        "Password must contain at least one lowercase letter, one uppercase letter, one digit, and be between 8 and 30 characters long."
+    }
   },
   restaurant_id: {
     type: Schema.Types.ObjectId,
     ref: "Restaurant",
-    required: true
+    required: [true, "Restaurant_id is required"]
   },
   phone: {
-    type: String
+    type: String,
+    required: [true, "Phone is required"]
   },
   email: {
-    type: String
+    type: String,
+    required: [true, "Email is required"],
+    unique: true
   },
   address: {
-    type: String
+    type: String,
+    required: [true, "Address is required"]
   },
   picture: {
     type: String
