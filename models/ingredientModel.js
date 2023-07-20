12d29c957a09c11979ecfd4b5f03c9fcf3f4c445
Khrystyna Krasovska <121851618+KrisKrasovska@users.noreply.github.com>
2023-07-20 10:43:13 +0000

    Delete joi schema and folder helpers

--- models/ingredientModel.js
+++ models/ingredientModel.js
@@ -1,12 +1,13 @@
 const { Schema, model } = require("mongoose");
-const Joi = require("joi");
-const { handleSchemaValidationErrors } = require("../helpers");
 
 const ingredientSchema = new Schema(
   {
     name: {
       type: String,
-      required: true
+      required: true,
+      match: /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      minlength: 3,
+      maxlength: 50
     },
     warehouseQuantity: {
       type: Number,
@@ -17,19 +18,6 @@ const ingredientSchema = new Schema(
     }
   });
 
-
-const joiSchema = Joi.object({
-  name: Joi.string().pattern(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/).min(3).max(50).required(),
-  warehouseQuantity: Joi.number().required(),
-  type: Joi.string().min(3).max(100),
-});
-
-ingredientSchema.post("save", handleSchemaValidationErrors);
-
-const schema = {
-  joiSchema,
-};
-
 const Ingredient = model("ingredient", ingredientSchema);
 
-module.exports = {Ingredient, schema};
+module.exports = Ingredient;
