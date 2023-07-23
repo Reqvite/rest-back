0e1dd30a9d0ea13debdd90b4cdf899dad1eddfb2
Mike Tanchuk <volkermischa@gmail.com>
2023-07-23 21:44:45 +0300

    - unnecessary endpoints

--- controllers/TransactionsController.js
+++ controllers/TransactionsController.js
@@ -1,25 +1,7 @@
 const TransactionsController = {
-    getAll: (req, res) => {
-        res.send('Transactions');
-    },
     create: (req, res) => {
         res.send('Create transaction');
     },
-    update: (req, res) => {
-        res.send('Update transaction');
-    },
-    delete: (req, res) => {
-        res.send('Delete transaction');
-    },
-    getOne: (req, res) => {
-        res.send('Get one transaction by id');
-    },
-    getByType: (req, res) => {
-        res.send('Get transactions by type');
-    },
-    getByOrder: (req, res) => {
-        res.send('Get transactions by order id');
-    }
 }
 
 module.exports = TransactionsController;
--- routes/transactions.js
+++ routes/transactions.js
@@ -18,98 +18,6 @@ const router = express.Router();
  *       responses:
  *         '201':
  *           description: Transaction created
- *     get:
- *       tags:
- *         - Transactions
- *       summary: Returns a list of all transactions
- *       responses:
- *         '200':
- *           description: A list of transactions
- *           schema:
- *             type: array
- *             items:
- *               $ref: '#/definitions/Transaction'
- *
- *   /transactions/{transactionId}:
- *     get:
- *       tags:
- *         - Transactions
- *       summary: Returns a transaction by ID
- *       parameters:
- *         - in: path
- *           name: transactionId
- *           required: true
- *           type: string
- *       responses:
- *         '200':
- *           description: A transaction
- *           schema:
- *             $ref: '#/definitions/Transaction'
- *     put:
- *       tags:
- *         - Transactions
- *       summary: Updates a transaction
- *       parameters:
- *         - in: path
- *           name: transactionId
- *           required: true
- *           type: string
- *         - in: body
- *           name: transaction
- *           schema:
- *             $ref: '#/definitions/Transaction'
- *       responses:
- *         '200':
- *           description: Transaction updated
- *     delete:
- *       tags:
- *         - Transactions
- *       summary: Deletes a transaction
- *       parameters:
- *         - in: path
- *           name: transactionId
- *           required: true
- *           type: string
- *       responses:
- *         '200':
- *           description: Transaction deleted
- *
- *   /transactions/type/{type}:
- *     get:
- *       tags:
- *         - Transactions
- *       summary: Returns transactions by type
- *       parameters:
- *         - in: path
- *           name: type
- *           required: true
- *           type: string
- *           enum: ['cash', 'POS', 'online']
- *       responses:
- *         '200':
- *           description: A list of transactions
- *           schema:
- *             type: array
- *             items:
- *               $ref: '#/definitions/Transaction'
- *
- *   /transactions/order/{orderId}:
- *     get:
- *       tags:
- *         - Transactions
- *       summary: Returns transactions by order ID
- *       parameters:
- *         - in: path
- *           name: orderId
- *           required: true
- *           type: string
- *       responses:
- *         '200':
- *           description: A list of transactions
- *           schema:
- *             type: array
- *             items:
- *               $ref: '#/definitions/Transaction'
  *
  * definitions:
  *   Transaction:
@@ -131,13 +39,7 @@ const router = express.Router();
  *         description: The type of transaction.
  */
 
-
-router.get('/', transactionsController.getAll);
 router.post('/', transactionsController.create);
-router.put('/:id', transactionsController.update);
-router.delete('/:id', transactionsController.delete);
-router.get('/:id', transactionsController.getOne);
-router.get('/type/:type', transactionsController.getByType);
-router.get('/order/:orderId', transactionsController.getByOrder);
+
 
 module.exports = router;
