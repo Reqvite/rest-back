d2dacb062939efceac8c08b17d9c5378cebdef4e
Lysak-Anna <hvlysak@gmail.com>
2023-07-25 16:37:59 +0300

    ADD: docs

--- utils/swagger.js
+++ utils/swagger.js
@@ -1,21 +1,21 @@
-const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerJSDoc = require("swagger-jsdoc");
 
 //options for the swagger docs
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Express API with Swagger',
-            description: 'This is a simple Express server',
-            version: '1.0.0',
-        },
-        servers: [
-            {
-                url: 'http://localhost:3000',
-            },
-        ],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Express API with Swagger",
+      description: "This is a simple Express server",
+      version: "1.0.0",
     },
-    apis: ['./routes/*.js', './schemas/*.js'], // files containing swagger annotations
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js", "./schemas/*.js"], // files containing swagger annotations
 };
 
 const specs = swaggerJSDoc(options);
