ca06e29eac4df95fbcf03b6de5ae3d08a11e4eac
github-marynapoliarush <merienf8118@gmail.com>
2023-07-26 13:08:29 +0300

    documentation add

--- routes/dishes.js
+++ routes/dishes.js
@@ -2,13 +2,90 @@ const express = require('express');
 const router = express.Router();
 const dishController = require ('../controllers/DishController')
 
-router.get('/:rest_id', dishController.getAllDishes)
-router.get('/dish/:id', dishController.getDishesById)
+/*
+  @openapi
+  paths:
+   /{rest_id}:
+    get:
+      summary: Get all dishes connected to selected restaurant
+       parameters:
+          - in: path
+           name: rest_id
+           required: true
+           type: string
+      responses:
+        '200':
+          description: Returns a list of dishes.
+           content:
+              application/json:
+                schema:
+                  type: array
+                  items:
+                    $ref: '#/components/schemas/Dish'
+  /dish/:                  
+   post:
+      summary: Add a new dish.
+      requestBody:
+         required: true
+         content:
+           application/json:
+              schema:
+                $ref: '#/components/schemas/Personnel'
+      responses:
+        '201':
+         description: Add dish. 
 
+
+  /dish/{id}:
+    get:
+      summary: Get exact dish.
+      parameters:
+          - in: path
+           name: dish_id
+           required: true
+           type: string
+      responses:
+        '200':
+          description: Returns an exact dish.
+           content:
+              application/json:
+                schema:
+                  type: array
+                  items:
+                    $ref: '#/components/schemas/Dish'
+
+    patch:
+      summary: Edit a dish by id.
+      parameters:
+          - in: path
+           name: dish_id
+           required: true
+           type: string
+      responses:
+        '204':
+         description: Dish edited.
+
+    delete:
+      summary: Delete a dish by id.
+      parameters:
+          - in: path
+           name: dish_id
+           required: true
+           type: string
+      responses:
+        '204':
+         description: Dish deleted.
+ */
+
+router.get('/:rest_id', dishController.getAllDishes)
 router.post('/dish/', dishController.addDish)
 
+router.get('/dish/:id', dishController.getDishesById)
 router.patch('/dish/:id', dishController.editDishById)
-
 router.delete('/dish/:id', dishController.deleteDishById)
+
+
+
+
 
 module.exports = router;
