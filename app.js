d0a0cfb453141d6e0c429e7354ba7fe14118af21
ValentinaDej <107748584+ValentinaDej@users.noreply.github.com>
2023-07-26 09:22:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const logger = require('morgan');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpecs = require('./utils/swagger');
 const connectDB = require('./db');
+const cors = require('cors');
+
+require('dotenv').config();
 
 //routes imports for the different parts of the application
 const indexRouter = require('./routes/index');
@@ -15,14 +18,17 @@ const restaurantsRoute = require('./routes/restaurants');
 const administratorsRoute = require('./routes/administrators');
 const waitersRoute = require('./routes/waiters');
 const transactionsRoute = require('./routes/transactions');
+const ingredientsRoute = require('./routes/ingredients');
+const uploadRoute = require('./routes/upload');
 const tablessRoute = require('./routes/tables');
 
 let app = express();
 
-//connect to the database
+// connect to the database
 try {
   connectDB();
-} catch (err) {
+}
+catch (err) {
   console.log(err);
 }
 
@@ -31,6 +37,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
 //routes
 app.use('/', indexRouter);
@@ -40,17 +47,20 @@ app.get('/api-docs', swaggerUi.setup(swaggerSpecs));
 app.use('/healthcheck', healthcheckRoute);
 
 app.use('/users', usersRouter);
-app.use('/restaurants', restaurantsRoute);
-app.use('/administrators', administratorsRoute);
-app.use('/waiters', waitersRoute);
-app.use('/transactions', transactionsRoute);
+app.use(`/restaurants`, restaurantsRoute);
+app.use(`/administrators`, administratorsRoute);
+app.use(`/waiters`, waitersRoute);
+app.use(`/transactions`, transactionsRoute);
+app.use(`/ingredients`, ingredientsRoute);
 app.use('/tables', tablessRoute);
+
+app.use(`/api`, uploadRoute)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3001, () => console.log('Example app listening on port 3001!'));
 
 module.exports = app;
