d5c2d9147f66200c13605cc46a9bdb76030905bc
Maksym Drobot <drobotm.com@gmail.com>
2023-07-30 20:11:51 +0300

    pulled changes from master

--- controllers/RestaurantsController.js
+++ controllers/RestaurantsController.js
@@ -1,7 +1,18 @@
+const { Restaurant } = require("../models")
+
 const restaurantsController = {
-  getRestaurantById: (req, res) => {
-    res.send('Restaurant data');
-  },
+  getRestaurantById: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const restaurant = await Restaurant.findById(id);
+      console.log(restaurant)
+
+      res.status(200).json(restaurant);
+    } catch (error) {
+      res.status(404).json({ error });
+    }
+  }
 };
 
 module.exports = restaurantsController;
