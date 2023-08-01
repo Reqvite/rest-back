24c82db3f1b7087bc4e56e5dceb63f83a49cdb5a
Vladyslav Yachyn <reqvite2233@gmail.com>
2023-08-01 17:25:08 +0300

    add async to transactionController

--- controllers/TransactionsController.js
+++ controllers/TransactionsController.js
@@ -1,15 +1,12 @@
-const { Order, Transaction } = require("../models");
-const LiqPayService = require("../services/liqpay/liqpayService");
+const { Order, Transaction } = require('../models');
+const LiqPayService = require('../services/liqpay/liqpayService');
+const asyncErrorHandler = require('../utils/errors/asyncErrorHandler');
 
 const TransactionsController = {
-  create: async (req, res) => {
+  create: asyncErrorHandler(async (req, res) => {
     const { amount, order_id, type, info } = req.body;
 
-    const paymentInfo = LiqPayService.getLiqPayPaymentData(
-      amount,
-      order_id,
-      info
-    );
+    const paymentInfo = LiqPayService.getLiqPayPaymentData(amount, order_id, info);
 
     if (!(await Transaction.findOne({ order_id }))) {
       await Transaction.create({
@@ -20,26 +17,26 @@ const TransactionsController = {
       });
     }
 
-    res.status(201).json({ status: "succes", code: 201, paymentInfo });
-  },
-  updateStatus: async (req, res) => {
+    res.status(201).json({ status: 'succes', code: 201, paymentInfo });
+  }),
+  updateStatus: asyncErrorHandler(async (req, res) => {
     const { data, signature } = req.body;
     const { status, info } = LiqPayService.getPaymentStatus(data, signature);
 
     const updatedOrders = await Order.updateMany(
-      { _id: { $in: info.split(",") } },
-      { status: "Paid" },
+      { _id: { $in: info.split(',') } },
+      { status: 'Paid' },
       { new: true }
     );
 
     return res.status(200).json({
       code: 200,
-      status: "success",
+      status: 'success',
       data: {
         updatedOrders,
       },
     });
-  },
+  }),
 };
 
 module.exports = TransactionsController;
