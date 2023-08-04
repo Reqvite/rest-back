42bedc27c89ad9a6129ee624f8f972dca28c6d1d
Mike Tanchuk <volkermischa@gmail.com>
2023-08-04 14:05:53 +0300

    restaurantId is now validating for existance. Added logic for changing the password with the ability to save the previous. Fixed pictures update.

--- controllers/PersonnelController.js
+++ controllers/PersonnelController.js
@@ -1,163 +1,190 @@
 const bcrypt = require('bcrypt');
 const s3 = require('@aws-sdk/client-s3');
 const presigner = require('@aws-sdk/s3-request-presigner');
-const { AuthorizationError, NotFoundError, BadRequestError } = require('../utils/errors/CustomErrors');
+const {AuthorizationError, NotFoundError, BadRequestError} = require('../utils/errors/CustomErrors');
 const Personnel = require('../models/personnelModel');
+const Restaurant = require('../models/restaurantModel');
 const asyncErrorHandler = require('../utils/errors/asyncErrorHandler');
-const { StatusCodes } = require('http-status-codes');
-const { OK, CREATED } = StatusCodes;
+const {StatusCodes, NOT_FOUND} = require('http-status-codes');
+const {OK, CREATED} = StatusCodes;
 
 const s3Client = new s3.S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
 });
 
 const personnelController = {
-  getPersonnelByRestaurantId: asyncErrorHandler(async (req, res, next) => {
-    const personnel = await Personnel.find({ restaurant_id: req.params.id });
+    getPersonnelByRestaurantId: asyncErrorHandler(async (req, res, next) => {
+        const personnel = await Personnel.find({restaurant_id: req.params.id});
 
-    for (const person of personnel) {
-      if (!person.picture) {
-        person.picture = 'RESTio.png';
-      }
-      const getObjectParams = {
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: person.picture,
-      };
-      const command = new s3.GetObjectCommand(getObjectParams);
-      person.picture = await presigner.getSignedUrl(s3Client, command, { expiresIn: 3600 });
-    }
+        /*for (const person of personnel) {
+          if (!person.picture) {
+            person.picture = 'RESTio.png';
+          }
+          const getObjectParams = {
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: person.picture,
+          };
+          const command = new s3.GetObjectCommand(getObjectParams);
+          person.picture = await presigner.getSignedUrl(s3Client, command, { expiresIn: 3600 });
+        }*/
 
-    if (!personnel) {
-      const err = new BadRequestError('Bad request');
-      return next(err);
-    }
+        if (!personnel) {
+            const err = new BadRequestError('Bad request');
+            return next(err);
+        }
 
-    res.status(OK).json(personnel);
-  }),
+        res.status(OK).json(personnel);
+    }),
 
-  getPersonnelById: asyncErrorHandler(async (req, res) => {
-    const personnel = await Personnel.findById(req.params.id);
-    console.log(personnel);
+    getPersonnelById: asyncErrorHandler(async (req, res) => {
+        const personnel = await Personnel.findById(req.params.id);
+        console.log(personnel);
 
-    if (!personnel.picture) {
-      personnel.picture = 'RESTio.png';
-    }
-    const getObjectParams = {
-      Bucket: process.env.AWS_BUCKET_NAME,
-      Key: personnel.picture,
-    };
-    const command = new s3.GetObjectCommand(getObjectParams);
-    personnel.picture = await presigner.getSignedUrl(s3Client, command, { expiresIn: 3600 });
+        /*if (!personnel.picture) {
+          personnel.picture = 'RESTio.png';
+        }
+        const getObjectParams = {
+          Bucket: process.env.AWS_BUCKET_NAME,
+          Key: personnel.picture,
+        };
+        const command = new s3.GetObjectCommand(getObjectParams);
+        personnel.picture = await presigner.getSignedUrl(s3Client, command, { expiresIn: 3600 });
+    */
+        if (!personnel) {
+            const err = new NotFoundError('Personnel with that ID is not found!');
+            return next(err);
+        }
 
-    if (!personnel) {
-      const err = new NotFoundError('Personnel with that ID is not found!');
-      return next(err);
-    }
+        res.status(OK).json(personnel);
+    }),
 
-    res.status(OK).json(personnel);
-  }),
+    addPersonnel: asyncErrorHandler(async (req, res) => {
+        const {
+            firstName,
+            lastName,
+            password,
+            gender,
+            role,
+            restaurant_id,
+            phone,
+            email,
+            address,
+            picture,
+        } = req.body;
 
-  addPersonnel: asyncErrorHandler(async (req, res) => {
-    const {
-      firstName,
-      lastName,
-      password,
-      gender,
-      role,
-      restaurant_id,
-      phone,
-      email,
-      address,
-      picture,
-    } = req.body;
+        const restaurant = await Restaurant.findById(restaurant_id);
 
-    // Hash the password using bcrypt
-    const hashedPassword = await bcrypt.hash(password, 13);
+        if (!restaurant) {
+            const err = new NotFoundError('Restaurant with that ID is not found!');
+            return res.status(NOT_FOUND).json({error: err.message});
+        }
 
-    // Create the personnel object to save to the database
-    const newPersonnel = new Personnel({
-      name: `${firstName} ${lastName}`,
-      password: hashedPassword,
-      restaurant_id,
-      gender,
-      role,
-      phone,
-      email,
-      address,
-      picture,
-    });
+        // Hash the password using bcrypt
+        const hashedPassword = await bcrypt.hash(password, 13);
 
-    console.log(newPersonnel);
-    // Save the personnel data to the database
-    const savedPersonnel = await newPersonnel.save();
+        // Create the personnel object to save to the database
+        const newPersonnel = new Personnel({
+            name: `${firstName} ${lastName}`,
+            password: hashedPassword,
+            restaurant_id,
+            gender,
+            role,
+            phone,
+            email,
+            address,
+            picture,
+        });
 
-    res.status(CREATED).json(savedPersonnel);
-  }),
+        console.log(newPersonnel);
+        // Save the personnel data to the database
+        const savedPersonnel = await newPersonnel.save();
 
-  updatePersonnel: asyncErrorHandler(async (req, res, next) => {
-    const { firstName, lastName, gender, phone, role, email, address, picture, restaurant_id } =
-      req.body;
-    const personnelId = req.params.id;
+        res.status(CREATED).json({message: 'Personnel added successfully!'});
+    }),
 
-    // Find the personnel by ID
-    const personnel = await Personnel.findById(personnelId);
+    updatePersonnel: asyncErrorHandler(async (req, res, next) => {
+        const {firstName, lastName, gender, password, phone, role, email, address, picture, restaurant_id} =
+            req.body;
+        const personnelId = req.params.id;
 
-    if (!personnel) {
-      const err = new NotFoundError('Personnel with that ID is not found!');
-      return next(err);
-    }
+        // Find the personnel by ID
+        const personnel = await Personnel.findById(personnelId);
 
-    //Take a restaurant id from the request body and check if it matches the rest id of the personnel
-    if (personnel.restaurant_id.toString() !== restaurant_id) {
-      console.log(personnel.restaurant_id.toString());
-      console.log(restaurant_id);
-      const err = new AuthorizationError();
-      return next(err);
-    }
+        if (!personnel) {
+            const err = new NotFoundError('Personnel with that ID is not found!');
+            return next(err);
+        }
 
-    // Update the personnel data
-    personnel.name = `${firstName} ${lastName}`;
-    personnel.gender = gender;
-    personnel.phone = phone;
-    personnel.role = role;
-    personnel.email = email;
-    personnel.address = address;
-    personnel.picture = picture;
+        const restaurant = await Restaurant.findById(restaurant_id);
 
-    // Save the updated personnel data to the database
-    const updatedPersonnel = await personnel.save();
+        if (!restaurant) {
+            const err = new NotFoundError('Restaurant with that ID is not found!');
+            return res.status(NOT_FOUND).json({error: err.message});
+        }
 
-    res.status(OK).json(updatedPersonnel);
-  }),
+        //Take a restaurant id from the request body and check if it matches the rest id of the personnel
+        if (personnel.restaurant_id.toString() !== restaurant_id) {
+            console.log(personnel.restaurant_id.toString());
+            console.log(restaurant_id);
+            const err = new AuthorizationError();
+            return next(err);
+        }
 
-  deletePersonnel: asyncErrorHandler(async (req, res, next) => {
-    const personnelId = req.params.id;
+        // Update the personnel data
+        personnel.name = `${firstName} ${lastName}`;
+        personnel.gender = gender;
+        personnel.phone = phone;
+        personnel.role = role;
+        personnel.email = email;
+        personnel.address = address;
+        if (picture) {
+            personnel.picture = picture;
+        }
+        if (password) {
+            personnel.password = await bcrypt.hash(password, 13);
+        }
 
-    // Find the personnel by ID
-    const personnel = await Personnel.findById(personnelId);
+        // Save the updated personnel data to the database
+        const updatedPersonnel = await personnel.save();
 
-    if (!personnel) {
-      const err = new NotFoundError('Personnel with that ID is not found!');
-      return next(err);
-    }
+        res.status(OK).json({message: 'Personnel updated successfully!'});
+    }),
 
-    //Take a restaurant id from the request body and check if it matches the rest id of the personnel
-    if (personnel.restaurant_id.toString() !== req.body.restaurant_id) {
-      console.log(personnel.restaurant_id.toString());
-      console.log(req.body.restaurant_id);
-      const err = new AuthorizationError();
-      return next(err);
-    }
+    deletePersonnel: asyncErrorHandler(async (req, res, next) => {
+        const personnelId = req.params.id;
+        const restaurant_id = req.body.restaurant_id;
+        // Find the personnel by ID
+        const personnel = await Personnel.findById(personnelId);
 
-    // Delete the personnel from the database
-    await personnel.deleteOne();
+        if (!personnel) {
+            const err = new NotFoundError('Personnel with that ID is not found!');
+            return next(err);
+        }
 
-    res.status(OK).json({ message: 'Personnel deleted successfully' });
-  }),
+        const restaurant = await Restaurant.findById(restaurant_id);
+
+        if (!restaurant) {
+            const err = new NotFoundError('Restaurant with that ID is not found!');
+            return res.status(NOT_FOUND).json({error: err.message});
+        }
+
+        //Take a restaurant id from the request body and check if it matches the rest id of the personnel
+        if (personnel.restaurant_id.toString() !== restaurant_id) {
+            console.log(personnel.restaurant_id.toString());
+            console.log(restaurant_id);
+            const err = new AuthorizationError();
+            return next(err);
+        }
+
+        // Delete the personnel from the database
+        await personnel.deleteOne();
+
+        res.status(OK).json({message: 'Personnel deleted successfully'});
+    }),
 };
 
 module.exports = personnelController;
