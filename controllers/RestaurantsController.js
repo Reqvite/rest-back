8b271b91bcf23949d64d4173cce87cd15eb55e40
Vladyslav Yachyn <reqvite2233@gmail.com>
2023-08-13 17:37:54 +0300

    Update RestaurantsController.js

--- controllers/RestaurantsController.js
+++ controllers/RestaurantsController.js
@@ -34,69 +34,122 @@ const restaurantsController = {
         },
       },
       {
-        $group: {
-          _id: {
-            month: {
-              $month: '$createdAt',
-            },
-          },
-          amount: {
-            $sum: '$paymentAmount',
-          },
-          transactions: {
-            $sum: 1,
-          },
-          online: {
-            $sum: {
-              $cond: [
-                {
-                  $eq: ['$type', 'online'],
+        $facet: {
+          monthlyData: [
+            {
+              $group: {
+                _id: {
+                  month: {
+                    $month: '$createdAt',
+                  },
                 },
-                1,
-                0,
-              ],
-            },
-          },
-          pos: {
-            $sum: {
-              $cond: [
-                {
-                  $eq: ['$type', 'POS'],
+                amount: {
+                  $sum: '$paymentAmount',
                 },
-                1,
-                0,
-              ],
-            },
-          },
-          cash: {
-            $sum: {
-              $cond: [
-                {
-                  $eq: ['$type', 'cash'],
+                transactions: {
+                  $sum: 1,
                 },
-                1,
-                0,
-              ],
+                online: {
+                  $sum: {
+                    $cond: [
+                      {
+                        $eq: ['$type', 'online'],
+                      },
+                      1,
+                      0,
+                    ],
+                  },
+                },
+                pos: {
+                  $sum: {
+                    $cond: [
+                      {
+                        $eq: ['$type', 'POS'],
+                      },
+                      1,
+                      0,
+                    ],
+                  },
+                },
+                cash: {
+                  $sum: {
+                    $cond: [
+                      {
+                        $eq: ['$type', 'cash'],
+                      },
+                      1,
+                      0,
+                    ],
+                  },
+                },
+              },
             },
-          },
-        },
-      },
-      {
-        $sort: {
-          '_id.month': 1,
-        },
-      },
-      {
-        $project: {
-          _id: 0,
-          name: {
-            $arrayElemAt: [monthNames, { $subtract: ['$_id.month', 1] }],
-          },
-          amount: 1,
-          transactions: 1,
-          online: 1,
-          pos: 1,
-          cash: 1,
+            {
+              $sort: {
+                '_id.month': 1,
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                name: {
+                  $arrayElemAt: [monthNames, { $subtract: ['$_id.month', 1] }],
+                },
+                amount: 1,
+                transactions: 1,
+                online: 1,
+                pos: 1,
+                cash: 1,
+              },
+            },
+          ],
+          timeBasedData: [
+            {
+              $group: {
+                _id: null,
+                totalToday: {
+                  $sum: {
+                    $cond: [
+                      { $gte: ['$createdAt', new Date(new Date().setHours(0, 0, 0, 0))] },
+                      '$paymentAmount',
+                      0,
+                    ],
+                  },
+                },
+                totalThisWeek: {
+                  $sum: {
+                    $cond: [
+                      {
+                        $and: [
+                          { $gte: ['$createdAt', new Date(new Date() - 7 * 24 * 60 * 60 * 1000)] },
+                          { $lt: ['$createdAt', new Date()] },
+                        ],
+                      },
+                      '$paymentAmount',
+                      0,
+                    ],
+                  },
+                },
+                totalThisMonth: {
+                  $sum: {
+                    $cond: [
+                      { $gte: ['$createdAt', new Date(new Date().setDate(1))] },
+                      '$paymentAmount',
+                      0,
+                    ],
+                  },
+                },
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                totalToday: { $round: ['$totalToday', 2] },
+                totalThisWeek: { $round: ['$totalThisWeek', 2] },
+                totalThisMonth: { $round: ['$totalThisMonth', 2] },
+              },
+            },
+          ],
         },
       },
     ];
