d9fcc53607042b8cb875cb5360d16f2ceaf68e98
MikeTanch <volkermischa@gmail.com>
2023-08-16 01:17:13 +0300

    Prompt improved. Added the feature for getting the Ids of CHATGPT`s suggestions. And sending it to front.

--- routes/openai.js
+++ routes/openai.js
@@ -1,10 +1,33 @@
+const Dish = require("../models/dishModel");
+
 const NotFoundError = require("../utils/errors/CustomErrors").NotFoundError;
 const Restaurant = require("../models/restaurantModel");
-const Ingredient = require("../models/restaurantModel");
-
 require("dotenv").config();
 const express = require("express");
 const router = express.Router();
+
+// Fetch all dishes by their IDs and populate ingredients
+const fetchDishes = async (dishIdsArray) => {
+    const fetchedDishes = [];
+
+    for (const dishId of dishIdsArray) {
+        try {
+            const dish = await Dish.findById(dishId).populate({path: 'ingredients', model: 'Ingredient'});
+
+            if (!dish) {
+                // Handle the case where a dish is not found
+                console.log(`Dish not found for dish ID: ${dishId}`);
+            } else {
+                fetchedDishes.push(dish);
+            }
+        } catch (error) {
+            // Handle any errors that occur during fetching
+            console.error(`Error fetching dish with ID ${dishId}:`, error);
+        }
+    }
+
+    return fetchedDishes;
+};
 
 router.post("/:id", async (req, res) => {
     try {
@@ -13,7 +36,7 @@ router.post("/:id", async (req, res) => {
         const {isVegan, likeSpicy, isPasc, wantHealthy, wantDrink} = req.body;
         const budget = parseInt(req.body.budget);
 
-        if (budget < 6) {
+        if (budget < 10) {
             const err = new NotFoundError('Budget is too small!');
             return res.status(err.statusCode).json({message: NotFoundError.message});
         }
@@ -27,22 +50,22 @@ router.post("/:id", async (req, res) => {
 
         const restaurants = await Restaurant.findById(restaurantId).populate({
             path: 'dishes_ids',
-            select: 'name picture portionWeight price ingredients type isActive',
-            populate: {
-                path: 'ingredients',
-                model: Ingredient,
-                select: 'name',
-            },
+            select: 'name price',
         });
 
         const dishes = restaurants.dishes_ids;
+
+        console.log('dishes', dishes);
 
         let prompt = `Hello, I want to eat in the restaurant and would like you to help me.
             I want to have a perfect meal. I will provide you with the data about the dishes 
             in the restaurant and my preferences as well as my budget. I want you to choose the dishes
              that can fell into my budget and satisfy my preferences. If there is no such an option, pls tell me that 
-             with such a budget I can not have a perfect meal in the restaurant. The price for every meal is shown in the 
-             price field in the menu.
+             with such a budget I can not have a perfect meal in the restaurant. I will provide you the JSON objects
+              of the menu: it will include the such fields: "name", "price", "_id". The price for every meal is shown in the 
+             price field in the menu. And please use you intelligence to define dishes my their names. All the dishes 
+             that will be present in menu are real dishes and you can find them in the internet or you must know 
+             about them.
             Here is an array of json objects of the menu of the restaurant I am going to eat in: ${dishes}.`;
 
         if (isVegan) {
@@ -66,9 +89,20 @@ router.post("/:id", async (req, res) => {
         }
 
         prompt = `${prompt} My budget is ${budget}$. Also, if there is an option when only one dish can fit my budget, 
-        pls give me this option. In your response, can you pls add break lines between paragraphs. Help me to choose the dishes.`;
+        pls give me this option. In your response, can you pls add break lines between paragraphs.
+        Help me to choose the dishes. I want the response to look in specific way. There should be 2 paragraphs. 
+        In the first, there should be your advices and the text that you consider as important. You should tell me 
+        the best set of dishes and your other comments. The second, I want it to start with the word "LIST_OF_DISHES"
+        and then there should be an array of dishes _ids. Please, do not
+        write ANY other text in the second paragraph. I want to have only the word "LIST_OF_DISHES" and the array of
+        dishes _ids. All the text and suggestions should be in the first paragraph. The previous sentance should end 
+        with a dot. The second paragraph should start with the word
+        "LIST_OF_DISHES" and after the array of dishes _ids. Please no more words before or after it. Please remove all
+        the "\n" symbols from the text. I want to have only one "\n" symbol between the paragraphs. Also, put only 
+        _id symbols without the word "ObjectId" before them. I want to have only the _id symbols. 
+        The example: [ "64d9ced6ec2a03efaaad15b0", "64da0f174574e1e22e9c0a5d" ].`;
 
-        console.log(prompt);
+        console.log('prompt', prompt);
 
         const api = new ChatGPTAPI({
             apiKey: process.env.OPENAI_API_KEY,
@@ -76,7 +110,44 @@ router.post("/:id", async (req, res) => {
 
         });
         const response = await api.sendMessage(prompt);
-        res.status(200).json({message: "Success", response});
+
+        /*const response = {
+            text: `Based on your budget of $146 and the menu options provided, I have selected the following dishes that satisfy your preferences: - Pizza Chelentano ($15) - Olivier2 ($2) These dishes fit within your budget and offer a good variety. However, please note that the Salad option exceeds your budget. LIST_OF_DISHES [ "64d9ced6ec2a03efaaad15b0", "64da0f174574e1e22e9c0a5d" ]`
+        }*/
+
+        console.log(response);
+
+        let responseText = response.text;
+
+        // Find the index of the occurrence of "LIST_OF_DISHES"
+        const index = responseText.indexOf("LIST_OF_DISHES");
+
+        // Extract the text before the occurrence
+        const textBefore = responseText.substring(0, index);
+
+        console.log('textBefore', textBefore);
+
+        responseText = responseText.replace(/\n/g, '');
+
+        console.log('responseText', responseText);
+
+        // Regular expression to match an array of IDs
+        const regex = /\[(.*?)\]/;
+
+        // Extract the matched array using RegExp
+        const match = responseText.match(regex);
+
+        if (match) {
+            const arrayOfIDs = JSON.parse("[" + match[1] + "]");
+            const dishes = await fetchDishes(arrayOfIDs);
+            console.log("Array of IDs found:", dishes);
+            res.status(200).json({textBefore, dishes});
+        } else {
+            console.log("No array of IDs found.");
+            res.status(200).json({textBefore, dishes: []});
+        }
+
+
     } catch (error) {
         console.error(error);
         res.status(500).json({message: "Something went wrong"});
