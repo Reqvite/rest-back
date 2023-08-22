8f62b5677ee4139ba0085a07edf9a06576d6f7c6
Maksym Drobot <drobotm.com@gmail.com>
2023-08-22 19:53:45 +0300

    test4 must be passed

--- db.js
+++ db.js
@@ -3,17 +3,19 @@ require('dotenv').config();
 
 const uri = process.env.ATLAS_URI;
 
+console.log('hello server');
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connection established successfully');
-    } catch (err) {
-        console.error('Error in DB connection: ' + err);
-        process.exit(1); // Exit the process with failure
-    }
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connection established successfully');
+  } catch (err) {
+    console.error('Error in DB connection: ' + err);
+    process.exit(1); // Exit the process with failure
+  }
 };
 
 module.exports = connectDB;
